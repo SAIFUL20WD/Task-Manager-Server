cd4b35147ed5aca1d1113d97b41bf8abd58860a5
saiful20wd <saiful2076af@gmail.com>
2024-03-10 23:07:44 +0600

    express rate limit fix

--- app.js
+++ app.js
@@ -30,11 +30,9 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(xss());
 
-const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 3000 });
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 1000, validate: {xForwardedForHeader: false, default: true} });
 app.use(limiter);
 
 app.use("/api/v1", router);
-
-app.get("/", (req, res) => res.send("Hello World"));
 
 module.exports = app;
